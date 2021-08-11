799a1e6f10140f9e9b5c721502ddf61daabe3a15
MrTechHunter <this.is.techhunter@gmail.com>
2021-08-11 10:03:40 +0430

    initial commit

--- src/pages/axios.tsx
+++ src/pages/axios.tsx
@@ -1,15 +1,40 @@
 import axios from "axios";
-import IUser from "../types/user";
+import { IUser } from "../types/user";
 // you don't need anything else to import
 
 interface Props {
   // TODO: add user type
+  user: IUser[];
 }
 
 export default function AxiosTest(props: Props) {
   // TODO: once you get data map through data and show them name
-  return <div>Simple Axios And Data Fetching :: Edit src/pages/axios.tsx</div>;
+
+  axios.get("/api/users").then((user) => {
+    console.log(user.data);
+    // props.user = user.data;
+  });
+
+  return (
+    <div>
+      Simple Axios And Data Fetching :: Edit src/pages/axios.tsx
+      <h2>Please look at the console, an object is there</h2>
+      <ul>
+        {/* {props.user.map((user) => {
+          return <h2 key={user}>{user.data}</h2>;
+        })} */}
+      </ul>
+    </div>
+  );
 }
+
+//SSR
+// https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering
+
+// export const getServerSideProps = async () => {
+//   const res = await axios.get("/api/users");
+//   return { props: res };
+// };
 
 // TODO: fetch data with axios to use in app
 // RestApi: axios.get("/api/users") => [{name: "amir"}]
--- src/pages/formik.tsx
+++ src/pages/formik.tsx
@@ -1,4 +1,6 @@
-import { Input, Form, Button, Space } from "antd";
+import { Form, Button, Space } from "antd";
+import { ErrorMessage, Formik, Field } from "formik";
+import * as Yup from "yup";
 
 const FormItem = Form.Item;
 
@@ -12,6 +14,15 @@ export default function FormikTest() {
   // BONUS: adding validation has extra points
   // Resource: https://formik.org/docs/tutorial
 
+  const signupFormSchema = Yup.object().shape({
+    username: Yup.string()
+      .required("This field is required")
+      .min(3, "This field must be at least 3 characters"),
+    password: Yup.string()
+      .required("This field is required")
+      .min(6, "This field must be at least 6 characters"),
+  });
+
   const handleSubmit = (values: IForm) => {
     console.log(values);
   };
@@ -21,28 +32,45 @@ export default function FormikTest() {
       <div>Simple Formik With Antd Inputs :: Edit src/pages/formik.tsx</div>
       <hr />
       {/* TODO: use Formik */}
-      <form>
-        {/* TODO: make this inputs work with formik i suggest make it a reusable component like FormikInput */}
-        <FormItem label="Username" name="username">
-          <Input />
-        </FormItem>
-        {/* TODO: make this inputs work with formik i suggest make it a reusable component like FormikInput */}
-        <FormItem label="Password" name="password">
-          <Input />
-        </FormItem>
-        <Space>
-          <Button type="primary" htmlType="submit">
-            submit
-          </Button>
-          <Button
-            onClick={() => {
-              // TODO: reset form with formik
-            }}
-          >
-            reset
-          </Button>
-        </Space>
-      </form>
+      <Formik
+        initialValues={{
+          username: "",
+          password: "",
+        }}
+        validationSchema={signupFormSchema}
+        onSubmit={handleSubmit}
+      >
+        <form id="signupForm">
+          {/* TODO: make this inputs work with formik i suggest make it a reusable component like FormikInput */}
+          <label htmlFor="username">Username </label>
+          <Field id="username" name="username" placeholder="mahdi" />
+          <ErrorMessage name="username" />
+
+          {/* TODO: make this inputs work with formik i suggest make it a reusable component like FormikInput */}
+          <br />
+          <br />
+          <label htmlFor="password">Password </label>
+          <Field id="password" name="password" placeholder="Qwerty12345" />
+          <ErrorMessage name="password" />
+
+          <br />
+          <br />
+          <Space>
+            <Button type="primary" htmlType="submit">
+              submit
+            </Button>
+            <Button
+              htmlType="reset"
+              onClick={() => {
+                // TODO: reset form with formik
+                resetForm;
+              }}
+            >
+              reset
+            </Button>
+          </Space>
+        </form>
+      </Formik>
     </div>
   );
 }
